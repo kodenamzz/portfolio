1e20b5ed3dd9093c0c9ac1d18b8ec38971c473f3
kodenamzz <kode4028@gmail.com>
2024-06-29 02:05:37 +0700

    add bento grid

---
+++ components/Grid.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item) => (
+          <BentoGridItem key={item.id} {...item} />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from "react-icons/fa";
+import { MagicButton } from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
@@ -34,6 +36,14 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Abdulkode, a Next.js Developer based Thailand
           </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position={"left"}
+            />
+          </a>
         </div>
       </div>
     </div>
